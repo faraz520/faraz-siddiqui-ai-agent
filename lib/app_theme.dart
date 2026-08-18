4e94d00281e962bf391be626e495ddc27f02d2e5
faraz520 <farazahmedsiddiqui55@gmail.com>
2026-08-18 09:33:24 +0500

    Place AppTheme in Flutter lib source directory

---
+++ lib/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF10B981);
+  static const Color darkBackground = Color(0xFF0F172A);
+  static const Color darkCardBackground = Color(0xFF1E293B);
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: darkBackground,
+      primaryColor: primaryColor,
+      cardTheme: CardThemeData(
+        color: darkCardBackground,
+        elevation: 2,
+      ),
+    );
+  }
+}
